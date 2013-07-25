d6912c0084d883e3b865ba99053c0a526a5eaea1
kscottz <katherineAScott@gmail.com>
2013-07-25 01:25:25 +0000

    moving stuff off the pi

--- server.py
+++ server.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 import pika
 import pygame as pg
-import pdb
+#import pdb
 
-pdb.set_trace()
+#pdb.set_trace()
 
-pg.mixer.pre_init()
+pg.mixer.pre_init(frequency=22050, size=-16, channels=1) # buffersize=4096)
 pg.init()
 
-#sound = pg.mixer.Sound('./evil.wav')
-#bear = pg.mixer.Sound('./bear.wav')
+sound = pg.mixer.Sound('./evil.wav')
+bear = pg.mixer.Sound('./bear.wav')
 #burp = pg.mixer.Sound('./burp.mp3')
-#goat = pg.mixer.Sound('./goat2.wav')
-#lroar = pg.mixer.Sound('./lion_roar.wav')
-#lgrowl = pg.mixer.Sound('./lion_growl.wav')
-#panther = pg.mixer.Sound('./panther.wav')
+goat = pg.mixer.Sound('./lion_roar.wav')
+lroar = pg.mixer.Sound('./lion_roar.wav')
+lgrowl = pg.mixer.Sound('./lion_growl.wav')
+panther = pg.mixer.Sound('./panther.wav')
 horse = pg.mixer.Sound('./evil.wav')
 
 soundMap = {
-#    'bear':bear,
-#    'burp':burp,
-#    'goat':goat,
-#    'lroar':lroar,
-#    'lgrowl':lgrowl,
-#    'panther':panther,
+    'bear':bear,
+    'burp':lgrowl,
+    'goat':lroar,
+    'lroar':lroar,
+    'lgrowl':lgrowl,
+    'panther':panther,
     'horse':horse
     }
 
